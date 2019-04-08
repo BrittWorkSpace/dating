435764b6dd31abd48269fbc162fdabcb97df447b
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-07 20:58:29 -0400

    Added comment headers for php and html. Added margins to indent quotes for html page

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author Michael Britt
+ * Date: 2/7/2019
+ * A basic php index page calling the home page for a dating website
+ * Includes the set up of our fate free framework
+ */
 //turn on error reporting
 
 ini_set('display_errors',1);
