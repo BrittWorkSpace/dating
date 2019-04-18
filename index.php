afad417c9f692e6f41905d65666ab3dec0c3ec99
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-17 23:53:27 -0400

    Updated files to hmtl/Add state array used in dropdown/ Added session data

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * @author Michael Britt
- * Date: 2/7/2019
+ * Date: 4/10/2019
  * A basic php index page calling the home page for a dating website
  * Includes the set up of our fate free framework
  */
-//turn on error reporting
 
+
+//turn on error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
@@ -17,6 +18,22 @@ require_once('vendor/autoload.php');
 //create an instance of the base class
 $f3 = Base::instance();
 
+//session starts
+session_start();
+
+//state array
+$states = array(
+    'AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California','CO' => 'Colorado',
+    'CT' => 'Connecticut', 'DE' => 'Delaware', 'DC' => 'District Of Columbia', 'FL' => 'Florida', 'GA' => 'Georgia',
+    'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas',
+    'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts',
+    'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana',
+    'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico',
+    'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma',
+    'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota',
+    'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington',
+    'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming'
+);
 //Define a default route
 $f3->route('GET /', function ()
 {
@@ -29,34 +46,48 @@ $f3->route('GET /', function ()
 $f3->route('GET|POST /personalInfo', function ()
 {
     $view = new Template();
-    echo $view->render('views/personalInfo.php');
+    echo $view->render('views/personalInfo.html');
 }
 );
 
 //profile enrty route
-$f3->route('GET|POST /profileEntry', function ()
-{
+$f3->route('GET|POST /profileEntry', function ($f3) use ($states) {
+    $_SESSION['fname'] = $_POST[fname];
+    $_SESSION['lname'] = $_POST[lname];
+    $_SESSION['age'] = $_POST[age];
+    $_SESSION['gender'] = $_POST[gender];
+    $_SESSION['phone'] = $_POST[phone];
+
+    $f3->set('states', $states);
     $view = new Template();
-    echo $view->render('views/profileEntry.php');
+    echo $view->render('views/profileEntry.html');
 }
 );
 //interests route
 $f3->route('GET|POST /interests', function ($f3)
 {
+    $_SESSION['email'] = $_POST[email];
+    $_SESSION['state'] = $_POST[state];
+    $_SESSION['seeking'] = $_POST[seeking];
+    $_SESSION['bio'] = $_POST[bio];
+
     $f3->set('indoor', array("tv" =>"Tv", "mov" => "Movies", "cook" => "Cooking", "board" => "Board Games", "puzz" => "Puzzles",
         "read" => "Reading", "card" => "Playing cards", "video" => "Video Games"));
     $f3->set('outdoor', array("hike" =>"Hiking", "bike" => "Biking", "swim" => "Swimming", "collect" => "Collecting",
     "walk" => "Walking", "climb" =>"Climbing"));
     $view = new Template();
-    echo $view->render('views/interests.php');
+    echo $view->render('views/interests.html');
 }
 );
 
 //profileSummary route
-$f3->route('GET|POST /profileSummary', function ()
+$f3->route('GET|POST /profileSummary', function ($f3)
 {
+    $_SESSION['indoor'] = $_POST[indoor];
+    $_SESSION['outdoor'] = $_POST[outdoor];
+    print_r($_SESSION);
     $view = new Template();
-    echo $view->render('views/profileSummary.php');
+    echo $view->render('views/profileSummary.html');
 }
 );
 
