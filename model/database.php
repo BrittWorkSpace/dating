2f7d571aee17d590a3d6ed350c042f345e458e58
Michael Britt <mbritt3@greenriver.mail.edu>
2019-05-13 13:42:48 -0400

    Add a database object and structure for methods required

---
+++ model/database.php
@@ -0,0 +1,29 @@
+<?php
+Class database
+{
+
+    public function connect()
+    {
+
+    }
+
+    public function insertMember()
+    {
+
+    }
+
+    public function getMembers()
+    {
+
+    }
+
+    public function getMember($member_id)
+    {
+
+    }
+
+    public function getInterests($member_id)
+    {
+
+    }
+}
